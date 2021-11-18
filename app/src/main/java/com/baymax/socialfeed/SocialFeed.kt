1aab46434641bf2ff875c9b1edbfa6c908416286
Abhinav <abhi2000.rtk@gmail.com>
2021-11-18 11:39:58 +0530

    basic di setup done

---
+++ app/src/main/java/com/baymax/socialfeed/SocialFeed.kt
@@ -0,0 +1,10 @@
+package com.baymax.socialfeed
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by abhinav on 18/11/21.
+ */
+@HiltAndroidApp
+class SocialFeed: Application()
---
+++ app/src/main/java/com/baymax/socialfeed/di/AppModule.kt
@@ -0,0 +1,16 @@
+package com.baymax.socialfeed.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+/**
+ * Created by abhinav on 18/11/21.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+
+
+}
